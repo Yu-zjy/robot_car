bcc54924fbaf6f12a50312efbf6122b9fca31c40
adventhunter <139053526+adventhunter@users.noreply.github.com>
2024-10-21 23:16:10 +0800

    Update create_object_db.py

--- create_object_db.py
+++ create_object_db.py
@@ -13,13 +13,10 @@ class ImageProcessor:
         self.image_sub = rospy.Subscriber('/usb_cam/image_raw', Image, self.image_callback)
 
     def image_callback(self, msg):
-        # 将 ROS 图像消息转换为 OpenCV 格式
         cv_image = self.bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
 
-        # 将图像转换为灰度图像
         gray_image = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
 
-        # 进行繁体中文识别
         text = pytesseract.image_to_string(gray_image, lang='chi_tra')  # 使用繁体中文识别
         rospy.loginfo(f"识别到的汉字: {text}")
 
